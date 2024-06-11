6a257cdacd7adde0c271d391a3b580b2ba0c74ad
Gabriel <gabriel42172332@gmail.com>
2024-06-11 13:40:48 -0300

    se agregó la inserción de la divisa

---
+++ ingresar_divisa.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="insertar_divisa.php" method="post">
+        <label for="d">divisa:</label>
+        <input type="text" name="d">
+        <input type="reset" value="Borrar">
+        <input type="submit" value="Guardar">
+    </form>
+</body>
+</html>
