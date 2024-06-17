7321cc86e7c75a3866bf493f09fa19b18c4adc5d
Gabriel <gabriel42172332@gmail.com>
2024-06-16 23:36:17 -0300

    Finalmente puede se puede ingresar una moneda

--- ingresar_moneda.php
+++ ingresar_moneda.php
@@ -16,6 +16,25 @@
             <input type="file" name="rvo" required>
         </p>
         <p>
+            <label for="nombre_moneda">Nombre:</label>
+            <input type="text" name="nombre_moneda" required>
+        </p>
+        <p>
+            <select name="tipo_moneda" id="">
+                <option selected disabled>--Seleccione el tipo de moneda--</option>
+                <?php 
+                include 'conexion.php';
+
+                $sql = "SELECT * FROM tipo_moneda";
+                $res = mysqli_query($conectar, $sql);
+                
+                foreach($res as $filas){
+                    echo "<option value='".$filas['id_tipo_moneda']."'>".$filas['tipo_moneda']."</option>";
+                }
+                ?>
+            </select>
+        </p>
+        <p>
             <select name="divisa" id="" required>
                 <option selected disabled>--Seleccione la divisa--</option>
                 <?php 
@@ -61,56 +80,6 @@
             <input type="text" name="historia" required>
         </p>
         <p>
-            <label for="ini_emi">Inicio emisión:</label>
-            <input type="date" name="ini_emi" required>
-        </p>
-        <p>
-            <label for="fin_emi">Fin emisión:</label>
-            <input type="date" name="fin_emi" required>
-        </p>
-        <p>
-            <label for="">Partes:</label>
-                <p>
-                <p>
-                    <select name="lado" required>
-                        <option value="" disabled selected>--Selecciona el lado--</option>
-                        <option value="Anverso">Anverso</option>
-                        <option value="Reverso">Reverso</option>
-                    </select>
-                </p>
-                <p>
-                    <label for="listel">Listel:</label>
-                    <input type="text" name="listel" required>
-                </p>
-                <p>
-                    <label for="efigie">Efigie:</label>
-                    <input type="text" name="efigie" required>
-                </p>
-                <p>
-                    <label for="leyenda">Leyenda:</label>
-                    <input type="text" name="leyenda" required>
-                </p>
-                <p>
-                    <label for="exergo">Exergo:</label>
-                    <input type="text" name="exergo" required>
-                </p>
-                <p>
-                    <label for="ley">Ley:</label>
-                    <input type="text" name="ley" required>
-                </p>
-                <p>
-                    <label for="grafilia">Grafilia:</label>
-                    <input type="text" name="grafilia" required>
-                </p>
-            </p>
-            <p>
-                <select name="circulacion" id="" required>
-                    <option value="" Selected disabled>--Circulación--</option>
-                    <option value="0">Verdadero</option>
-                    <option value="1">Falso</option>
-                </select>
-            </p>
-            <p>
                 <label for="composicion">Composición:</label>
                 <input type="text" name="composicion" id="" required>
             </p>
@@ -122,6 +91,81 @@
                 <label for="espesor">Espesor:</label>
                 <input type="number" name="espesor" required>
             </p>
+        <p>
+            <label for="ini_emi">Inicio emisión:</label>
+            <input type="date" name="ini_emi" required>
+        </p>
+        <p>
+            <label for="fin_emi">Fin emisión:</label>
+            <input type="date" name="fin_emi" required>
+        </p>
+        <p>
+            <h2>Partes:</h2>
+                <p>
+                    <h3>Anverso:</h3>
+                    <p>
+                        <p>
+                            <label for="listel">Listel:</label>
+                            <input type="text" name="listel_anver" required>
+                        </p>
+                        <p>
+                            <label for="efigie">Efigie:</label>
+                            <input type="text" name="efigie_anver" required>
+                        </p>
+                        <p>
+                            <label for="leyenda">Leyenda:</label>
+                            <input type="text" name="leyenda_anver" required>
+                        </p>
+                        <p>
+                            <label for="exergo">Exergo:</label>
+                            <input type="text" name="exergo_anver" required>
+                        </p>
+                        <p>
+                            <label for="ley">Ley:</label>
+                            <input type="text" name="ley_anver" required>
+                        </p>
+                        <p>
+                            <label for="grafilia">Grafilia:</label>
+                            <input type="text" name="grafilia_anver" required>
+                        </p>
+                        <p>
+                            <label for="detalles">Detalles:</label>
+                            <input type="text" name="detalles_anver">
+                        </p>
+                    </p>
+                    <br>
+                    <h3>Reverso:</h3>
+                    <p>
+                        <p>
+                            <label for="listel">Listel:</label>
+                            <input type="text" name="listel_rever" required>
+                        </p>
+                        <p>
+                            <label for="efigie">Efigie:</label>
+                            <input type="text" name="efigie_rever" required>
+                        </p>
+                        <p>
+                            <label for="leyenda">Leyenda:</label>
+                            <input type="text" name="leyenda_rever" required>
+                        </p>
+                        <p>
+                            <label for="exergo">Exergo:</label>
+                            <input type="text" name="exergo_rever" required>
+                        </p>
+                        <p>
+                            <label for="ley">Ley:</label>
+                            <input type="text" name="ley_rever" required>
+                        </p>
+                        <p>
+                            <label for="grafilia">Grafilia:</label>
+                            <input type="text" name="grafilia_rever" required>
+                        </p>
+                        <p>
+                            <label for="detalles">Detalles:</label>
+                            <input type="text" name="detalles_rever">
+                        </p>
+                    </p>
+                </p>
         </p>
         <input type="submit" value="Guardar">
     </form>
--- insertar_moneda.php
+++ insertar_moneda.php
@@ -7,20 +7,30 @@ error_reporting(E_ALL);
 $dvs = $_POST['divisa'];
 $v_n = $_POST['v_n']; //valor nominal
 $t_c = $_POST['t_c']; //tipo canto
-$ld = $_POST['lado'];
-$lstl = $_POST['listel'];
-$fg = $_POST['efigie'];
-$lynd = $_POST['leyenda'];
-$exergo = $_POST['exergo'];
-$ly = $_POST['ley'];
-$grfla = $_POST['grafilia'];
-$crclcn = $_POST['circulacion'];
+$n_m = $_POST['nombre_moneda'];
+$t_m = $_POST['tipo_moneda'];
 $cmpscn = $_POST['composicion'];
 $dmtr = $_POST['diametro'];
 $spsr = $_POST['espesor'];
 $hstr = $_POST['historia'];
-$nc_msn = $_POST['ini_emi'];
+$nc_msm = $_POST['ini_emi'];
 $fn_msm = $_POST['fin_emi'];
+
+$lstl_anver = $_POST['listel_anver'];
+$fg_anver = $_POST['efigie_anver'];
+$lynd_anver = $_POST['leyenda_anver'];
+$exergo_anver = $_POST['exergo_anver'];
+$ly_anver = $_POST['ley_anver'];
+$grfl_anver = $_POST['grafilia_anver'];
+$dtlls_anver = $_POST['detalles_anver'];
+
+$lstl_rever = $_POST['listel_rever'];
+$fg_rever = $_POST['efigie_rever'];
+$lynd_rever = $_POST['leyenda_rever'];
+$exergo_rever = $_POST['exergo_rever'];
+$ly_rever = $_POST['ley_rever'];
+$grfl_rever = $_POST['grafilia_rever'];
+$dtlls_rever = $_POST['detalles_rever'];
 
 $ruta_indexphp = dirname(realpath(__FILE__));
 $anverso = $_FILES['anv']['tmp_name'];
@@ -40,22 +50,54 @@ if(in_array($_FILES['anv']['type'], $extensiones)){
             include 'conexion.php';
             $sql = "INSERT INTO `imagen`(`direccion`) VALUES ('$anverso_destino')";
             $res = mysqli_query($conectar, $sql);
-            $id_img_anv = last_insert($res);
+            $id_img_anv = mysqli_insert_id($conectar);
             $sql = "INSERT INTO `imagen`(`direccion`) VALUES ('$reverso_destino')";
             $res = mysqli_query($conectar, $sql);
-            $id_img_rev = last_insert($res);
+            $id_img_rev = mysqli_insert_id($conectar);
 
             if($res){
-                $sql = "INSERT INTO 'moneda_atributos'(`id_divisa`, `id_valor_nominal`, 
-                        `id_tipo_canto`, `circulacion`, `composicion`, `diametro`, `espesor`, `historia`, `inicio_emision`,
-                        `fin_emision`)
-                        VALUES ('$dvs','$v_n','$t_c','$crclcn','$cmpscn','$dmtr', '$spsr','$hstr','$ini_emi','$fin_emi')";
+                $sql = "INSERT INTO `moneda`(`nombre`) VALUES ('$n_m')";
                 $res = mysqli_query($conectar, $sql);
+                $id_act = mysqli_insert_id($conectar);
 
                 if($res){
-                    $sql = "INSERT INTO 'partes'(`id_imagen`, `id_moneda_atributos`, `lado`, `listel`, `efigie`, `leyenda`, 
-                    `exergo`, `ley`, `grafilia`)
-                     VALUE ();";
+                    $sql = "INSERT INTO `moneda_atributo`(`id_moneda`, `id_divisa`, `id_valor_nominal`, `id_tipo_canto`, 
+                    `id_tipo_moneda`, `composicion`, `diametro`, `espesor`, `historia`, `inicio_emision`, `fin_emision`) 
+                    VALUES ($id_act , $dvs, $v_n, $t_c, $t_m,'$cmpscn','$dmtr','$spsr','$hstr','$nc_msm','$fn_msm')";
+                    $res = mysqli_query($conectar, $sql);
+                    $id_act = mysqli_insert_id($conectar);
+
+                    if($res){
+                        $sql = "INSERT INTO `partes`(`id_imagen`, `id_moneda_atributo`, `lado`, `listel`, `efigie`, 
+                        `leyenda`, `exergo`, `ley`, `grafilia`, `detalles`) 
+                        VALUES ($id_img_anv,$id_act,'anverso','$lstl_anver','$fg_anver','$lynd_anver','$exergo_anver'
+                        ,'$ly_anver','$grfl_anver','$dtlls_anver')";
+                        $res = mysqli_query($conectar, $sql);
+                        var_dump($sql);
+
+                        if($res){
+                            $sql = "INSERT INTO `partes`(`id_imagen`, `id_moneda_atributo`, `lado`, `listel`, `efigie`, 
+                            `leyenda`, `exergo`, `ley`, `grafilia`, `detalles`) 
+                            VALUES ($id_img_rev,$id_act,'reverso','$lstl_rever','$fg_rever','$lynd_rever','$exergo_rever'
+                            ,'$ly_rever','$grfl_rever','$dtlls_rever')";
+                            $res = mysqli_query($conectar, $sql);
+                            var_dump($sql);
+
+                            if($res){
+                                echo "<script>alert('La moneda se ingresó con éxito.');</script>";
+                            }else{
+                                echo "<script>alert('ERROR: La moneda no se pudo ingresar.');</script>";
+                            }
+
+                        }else{
+                            echo "<script>alert('ERROR: No se pudo insertar el anverso de la moneda');</script>";
+                        }
+                    }else{
+                        var_dump($sql);
+                        echo "<script>alert('ERROR: No se pudo insertar los atrbutos de la moneda');</script>";
+                    }
+                }else{
+                    echo "<script>alert('ERROR: No se pudo insertar el nombre de la moneda');</script>";
                 }
             }else{
                 echo "<script>alert('ERROR: No se pudo ingresar las imagenes de la moneda');</script>";
