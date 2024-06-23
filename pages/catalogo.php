bfdd4e05d2c61770c1f805b3b15f689db8ccfd9b
Gania <ganiavin.gv@gmail.com>
2024-06-23 00:47:44 -0300

    catálogo inicial sin estilo

--- pages/catalogo.php
+++ pages/catalogo.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/output.css">
     <title>Catálogo</title>
 </head>
 <body>
@@ -17,8 +18,12 @@
             </form>
         </span>
     </section>
+
     <section>
         <div>
+            <img src="img/10-Centavos.jpg" alt="moneda">
+            <h5>Moneda</h5>
+            <h6>1888-0012</h6>
             
         </div>
     </section>
