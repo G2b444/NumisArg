42047bffcd70f36aac49dcf5049d5c44bb751cf5
Gabriel <gabriel42172332@gmail.com>
2024-06-22 23:21:24 -0300

    Se arregló un mal redireccionamiento de la db en insertar moneda y se agregarón imagenes

--- views/admin-profile/insertar_moneda.php
+++ views/admin-profile/insertar_moneda.php
@@ -35,8 +35,8 @@ $dtlls_rever = $_POST['detalles_rever'];
 $ruta_indexphp = dirname(realpath(__FILE__));
 $anverso = $_FILES['anv']['tmp_name'];
 $reverso = $_FILES['rvo']['tmp_name'];
-$anverso_destino ='assets/multimedia/img/'. $_FILES['anv']['name'];
-$reverso_destino ='imagenes/'. $_FILES['rvo']['name'];
+$anverso_destino ='assets/img/'. $_FILES['anv']['name'];
+$reverso_destino ='assets/img/'. $_FILES['rvo']['name'];
 
 
 $extensiones = array(0=>'image/jpg', 1=>'image/jpeg', 2=>'image/png');
